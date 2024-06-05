cb6c2c0c864516dfa8fc1a2b5678f82242089782
GabrielValim <barros.gabriel.valim@hotmail.com>
2024-06-05 20:43:05 -0300

    exercicios for try except

---
+++ 1.py
@@ -0,0 +1,9 @@
+# 1 - Crie uma lista para cada informação a seguir:
+
+# Lista de números de 1 a 10;
+# Lista com quatro nomes;
+# Lista com o ano que você nasceu e o ano atual.
+
+list_numbers = [i for i in range(1, 11)];
+list_names = ["Ana", "João", "Maria", "Pedro"];
+lists_years = [2001, 2024];
---
+++ 2.py
@@ -0,0 +1,6 @@
+# 2 - Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+
+list_names = ["Ana", "João", "Maria", "Pedro"];
+
+for name in list_names:
+    print(name)
